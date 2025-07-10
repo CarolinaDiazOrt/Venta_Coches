5c3e325622b0b96be8abf403ce4cd2b3f893c43c
CarolinaDiazOrt <carodiazcolab@gmail.com>
2025-07-09 20:13:54 -0500

    Actualizar app

--- app.py
+++ app.py
@@ -9,14 +9,36 @@ fig2.show() # crear gráfico de dispersión
 
 st.header('Análisis de precios de vehículos')
 
-hist_button = st.button('Construir histograma') # crear un botón
+st.markdown("""
+Este proyecto realiza un análisis exploratorio de una base de datos con información sobre vehículos listados para la venta.
 
-if hist_button: # al hacer clic en el botón
+El objetivo principal es comprender el comportamiento del mercado de carros y analizar los factores que pueden influir en el precio final de un vehículo. Entre las variables consideradas se incluyen el año del modelo, la marca, el kilometraje, la condición del vehículo, el tipo de transmisión, entre otros.
+
+A continuación, elija la gráfica que desea visualizar.             
+            """)
+
+st.subheader('Distribuciones de variables')
+
+hist_button_odom = st.button('Construir histograma odómetro') # crear un botón
+
+if hist_button_odom: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el kilometraje de los coches en venta')
+    
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer") # crear histograma
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+hist_button_price = st.button('Construir histograma precio') # crear un botón
+
+if hist_button_price: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer") # crear histograma
+    fig = px.histogram(car_data, x="price") # crear histograma
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
@@ -28,7 +50,7 @@ if scatter_button: # al hacer clic en el botón
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
     
     # crear un histograma
-    fig = px.scatter(car_data, x="odometer") # crear gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price") # crear gráfico de dispersión
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
